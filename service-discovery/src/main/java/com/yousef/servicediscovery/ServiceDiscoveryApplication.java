02bde5758c637313e6011862dbdde8cc2469b6ff
azizbhk <146841217+azizbhk@users.noreply.github.com>
2024-11-25 20:24:15 +0100

    Update ServiceDiscoveryApplication.java

--- service-discovery/src/main/java/com/yousef/servicediscovery/ServiceDiscoveryApplication.java
+++ service-discovery/src/main/java/com/yousef/servicediscovery/ServiceDiscoveryApplication.java
@@ -1,4 +1,4 @@
-package com.yousef.servicediscovery;
+package com.aziz.servicediscovery;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
